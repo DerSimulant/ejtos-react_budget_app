6226fd3396883b15a96309f0b0208907b1dd9e85
DerSimulant <82386236+DerSimulant@users.noreply.github.com>
2022-12-22 15:08:04 +0100

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const {  remaining, dispatch, currency } = useContext(AppContext);
+    const [budget, setBudget] = useState('');
+
+    const handleChange = (event) => {
+        setBudget(event.target.value);
+      };
+
+      const handleKeyDown = (event) => {
+        if (event.key === 'Enter'){
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: budget,
+           });
+
+        if (remaining <= 0) {
+            alert("The budget is not allowed to be less than the spending ");
+            
+            return; }
+        
+        if (budget > 20000) {
+            alert("You are not allowed to allocate more than 20.000,-");
+            
+            return; }
+        
+    }
+    };
+    
+      //  return budget = event.target.value
+      //  }
+    //if(budget < remaining) {
+       // alert("The value cannot exceed remaining funds  £"+remaining);
+        //setBudget("");
+        //return;
+   // }
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}{<input
+                        type='number'
+                        id='budget'
+                        value={budget}
+                        style={{ marginLeft: '2rem' , size: 2}}
+                        onChange={handleChange}
+                        onKeyDown={handleKeyDown}>
+                        </input>}</span>
         </div>
     );
 };
